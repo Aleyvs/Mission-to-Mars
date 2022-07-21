084c39595052e41ae4a0be528cd141aac80eddbd
Alejandra Villarreal <alejandra.y.villarreal21@gmail.com>
2022-07-20 23:41:41 -0500

    changes to app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, redirect, render_template, url_for
 from flask_pymongo import PyMongo
+
 import scraping
 
 ## setiup Flask
